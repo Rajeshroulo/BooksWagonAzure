738afc819bac4159419aaa343b5049978def43da
Rajeshroulo <rajraval017@gmail.com>
2020-09-30 19:08:46 +0530

    Added code to search for a book

---
+++ BooksWagonAzure/Pages/BookSearchPage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+
+namespace Bookswagon.Page
+{
+    public class BookSearchPage
+    {
+        public IWebDriver driver;
+        public BookSearchPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='search-input']//input")]
+        public IWebElement search;
+
+        [FindsBy(How = How.Id, Using = "ctl00_TopSearch1_Button1")]
+        public IWebElement searchButton;
+        
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Bestsellers')]")]
+        public IWebElement wingsOfFire;
+
+        public void BookSearching()
+        {
+            Thread.Sleep(1000);
+            search.SendKeys(ConfigurationManager.AppSettings["BookName"]);
+            searchButton.Click();
+            Thread.Sleep(3000);
+            IList<IWebElement> books = driver.FindElements(By.XPath("//a[contains(text(),'Wings of Fire')]"));
+            foreach(var book in books)
+            {
+                Console.WriteLine(book.Text);
+            }
+            books[1].Click();
+            Thread.Sleep(3000);
+        }
+
+        public string BookTitle()
+        {
+            return wingsOfFire.Text;
+        }
+    }
+}
--- BooksWagonAzure/Test/BookswagonTests.cs
+++ BooksWagonAzure/Test/BookswagonTests.cs
@@ -8,12 +8,21 @@ namespace BooksWagonAzure.Test
     public class BookswagonTests : BaseClass
     {
         UserData data = new UserData();
-        [Test]
+        [Test, Order(1)]
         public void BookswagonLogin()
         {
             var login = new LoginPage(driver);
             login.AccountLogin(data.email, data.bookspassword);
             Assert.AreEqual("TextBooks", login.TextBooks());
+        }
+
+        [Test, Order(2)]
+        public void SearchBooks()
+        {
+            var search = new BookSearchPage(driver);
+            search.BookSearching();
+            string text = "Bestsellers";
+            Assert.AreEqual(text, search.BookTitle());
         }
 
     }
