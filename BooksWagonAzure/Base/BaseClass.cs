639dbbdb2cffd41dc4f2a70cdc5505f3b38c76c5
Rajeshroulo <rajraval017@gmail.com>
2020-10-02 12:51:56 +0530

    Modified the script for base class

--- BooksWagonAzure/Base/BaseClass.cs
+++ BooksWagonAzure/Base/BaseClass.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 using System.Configuration;
 
 namespace BooksWagonAzure.Base
@@ -15,6 +16,7 @@ namespace BooksWagonAzure.Base
             ChromeOptions options = new ChromeOptions();
             options.AddArguments("--disable-notifications", "start-maximized");
             driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             driver.Url = ConfigurationManager.AppSettings["URL"];
         }
 
